003b11882a569e62cbebf3711e91670bc3e70308
Adrace <91935027+Adrace@users.noreply.github.com>
2021-11-03 12:55:12 +0900

    Add files via upload

---
+++ doc/basic05.js
@@ -0,0 +1,19 @@
+$(function(){
+//---------------------------------------------------
+
+$('#top_banner i').on('click',function(){
+    $('#top_banner').slideUp();
+});
+
+$('#main_visual .main__slider').slick({
+    arrows:false,
+    autoplay:true,
+    pauseOnHover:true,
+    pauseOnFocus:false,
+});
+
+
+
+
+//---------------------------------------------------
+});
