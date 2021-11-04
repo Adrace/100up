486dc822a730d9fbf6df3b4d30c8be58019de222
Adrace <91935027+Adrace@users.noreply.github.com>
2021-11-04 15:04:18 +0900

    Add files via upload

--- doc/basic05.js
+++ doc/basic05.js
@@ -11,6 +11,42 @@ $('#main_visual .main__slider').slick({
     pauseOnHover:true,
     pauseOnFocus:false,
 });
+$('.pr_slider').slick({
+    arrows:false,
+    slidesToShow:5,
+    // centerMode:true,
+    // centerPadding:"100px",
+    autoplay:true,
+});
+
+$('.pr_slider').on('afterChange',function(e,s,c){
+    // console.log(c);
+    $('.pr_slider figure').eq(c+7).addClass('on').siblings().removeClass('on');
+    $('#product03 .slide_bar>span').css({left:c*100});
+});
+
+$('.pr_slider02').slick({
+    arrows:false,
+    autoplay:true,
+    slidesToShow:3,
+    slidespeed:2000,
+    pauseOnHover:true,
+    pauseOnFocus:false,
+});
+
+$('#product04 i.xi-angle-left').on('click',function(){
+    $('.pr_slider02').slick('slickPrev');
+});
+$('#product04 i.xi-angle-right').on('click',function(){
+    $('.pr_slider02').slick('slickNext');
+});
+
+
+$('#link select').on('change',function(){
+    var link=$(this).val();
+    if(link) {window.open(link);}
+
+});
 
 
 
