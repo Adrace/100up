355b650c17c91e1ec1b64b777d4b90ad83479af6
Adrace <91935027+Adrace@users.noreply.github.com>
2021-11-08 12:50:06 +0900

    Add files via upload

---
+++ doc/responsive01.js
@@ -0,0 +1,13 @@
+$(function(){
+//---------------------------------------------------
+
+$('#header i').on('click',function(){
+    $('#header .container nav').toggle();
+});
+
+$(widow).on('resize',function(){
+    $('nav').removeAttr('style');
+});
+
+//---------------------------------------------------
+});
