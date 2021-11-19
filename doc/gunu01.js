e92adcaf149517493703c8b788502117932001e0
Adrace <91935027+Adrace@users.noreply.github.com>
2021-11-19 11:13:55 +0900

    Add files via upload

---
+++ doc/gunu01.js
@@ -0,0 +1,19 @@
+$(function(){
+// --------------------------------------
+
+
+    $(".main_slider").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        nav:true,
+    });
+
+
+
+
+
+// --------------------------------------
+
+});
