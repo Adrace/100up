c5ef00c321027a3ecf2541d07e6a11484ed164e3
Adrace <91935027+Adrace@users.noreply.github.com>
2022-01-22 23:13:58 +0900

    Add files via upload

---
+++ portfolio03/js/main.js
@@ -0,0 +1,63 @@
+$(function(){
+// --------------------------------------------------------
+
+$('.fullmenu').on('click',function(){
+    $('.full_menu').addClass('on');
+});   
+$('.close').on('click',function(){
+    $('.full_menu').removeClass('on');
+});
+
+
+$('.header .rightmenu .lang>li').on('click',function(){
+    $(this).toggleClass('on');
+});
+
+$('.slide').slick({
+    arrows:false,
+    autoplay:true,
+    pasueOnHover:false,
+    dots:true,
+});
+
+
+$(".banner01").on('click',function(){
+    $('.banner01_sns').toggleClass('on');
+});
+$(".banner02").on('click',function(){
+    $('html,body').animate({scrollTop:0});
+});
+
+$('.header .mopen').on('click',function(){
+    $('.header .gnb').toggleClass('on');
+    $('.header .rightmenu').toggleClass('on');
+});
+
+
+
+$('.header .gnb>ul>li>a').on('click',function(){
+    if($('.header .gnb').hasClass('on')) {
+        $(this).next().stop().slideToggle();
+        $(this).parent().siblings().find('.sub_menu').slideUp();  
+    }
+});
+$('.header .sub_menu>li>a').on('click',function(){
+    if($('.header .gnb').hasClass('on')) {
+        $(this).next().stop().slideToggle();
+        $(this).parent().siblings().find('.ssub_menu').slideUp();  
+    }
+});
+
+$(window).on('resize',function(){
+    $('.header .sub_menu').removeAttr('style')
+    if ($(window).width() < 768) {
+        $('.full_menu').removeClass('on');
+    }
+});
+
+
+
+
+
+// --------------------------------------------------------
+});
